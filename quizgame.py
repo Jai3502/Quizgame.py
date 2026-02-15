e742ce64e37814495923b84293938f7a0daef5dc
jai <jaiprakash3502@gmail.com>
2026-02-15 17:11:32 +0530

    Add files via upload

---
+++ quizgame.py
@@ -0,0 +1,57 @@
+print("Welcome to quiz!")
+
+playing = input("Do you want to play? ")
+if playing != "yes":
+    quit()
+
+print("okay! let's play:)") 
+  
+answer = input("what does stand cpu for? ") 
+if answer == " central processing unit":
+    print("correct! ")
+else: 
+    print("incorrect! ")
+
+answer = input("what does stand fir for? ") 
+if answer == " First Information Report":
+    print("correct! ")
+else: 
+    print("incorrect! ")  
+    
+      
+answer = input("what does stand ca for? ") 
+if answer == " Chartered Accountant ":
+    print("correct! ")
+else: 
+    print("incorrect! ") 
+
+answer = input("what does stand gdp for? ") 
+if answer == "Gross domestic product  ":
+    print("correct! ")
+else: 
+    print("incorrect! ")
+
+    
+answer = input("what does stand Uk for? ") 
+if answer == " United kingdom ":
+    print("correct! ")
+else: 
+    print("incorrect! ")
+    
+answer = input("what does stand Rbi for? ") 
+if answer == "Reserve bank of India  ":
+    print("correct! ")
+else: 
+    print("incorrect! ")
+
+answer = input("what does stand Sebi for? ") 
+if answer == " securities and exchange board of India ":
+    print("correct! ")
+else: 
+    print("incorrect! ")    
+
+answer = input("what does stand Un for? ") 
+if answer == " United Nations ":
+    print("correct! ")
+else: 
+    print("incorrect! ")    
